5eab05d9aa9cc5ca793207701c495549aa6f6ddc
Elio1995 <mehmetielio95@gmail.com>
2022-07-26 14:54:33 +0200

    Fixed the styling of inside cards

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -12,29 +12,88 @@ function Card(props: any) {
               <img src={job.logo}></img>
             </div>
             <div className="text-start text-black">
-              <span>{job.company}</span>
-              <span>{job.new === true ? "NEW!" : ""}</span>
-              <span className="ml-5">
-                {job.featured === true ? "FEATURED" : ""}
+              <span
+                style={{ color: "hsl(180, 29%, 50%)" }}
+                className="font-black mr-3"
+              >
+                {job.company}
               </span>
-              <p>{job.position}</p>
-              <span>{job.postedAt}</span>
-              <span>{job.contract}</span>
-              <span>{job.location}</span>
+
+              {job.new === true ? (
+                <span
+                  style={{
+                    backgroundColor: "hsl(180, 29%, 50%)",
+                  }}
+                  className="px-2 py-1 rounded-2xl text-white font-black text-xs"
+                >
+                  NEW!
+                </span>
+              ) : (
+                <span></span>
+              )}
+              {job.featured === true ? (
+                <span className="bg-black rounded-2xl px-2 py-1 ml-2 text-white font-black text-xs">
+                  FEATURED
+                </span>
+              ) : (
+                <span></span>
+              )}
+              <p className="mt-2 text-lg font-black">{job.position}</p>
+              <div className="mt-2 text-xs font-semibold text-slate-400">
+                <span>{job.postedAt}</span>
+                <span className="mx-4">•</span>
+                <span>{job.contract}</span>
+                <span className="mx-4">•</span>
+                <span>{job.location}</span>
+              </div>
             </div>
-            <div className="text-start text-black">
-              <span>{job.role}</span>
-              <span>{job.level}</span>
-              <span>
-                {job.languages.map((language) => {
-                  return <>{language}</>;
-                })}
+            <div className="text-end text-black">
+              <span
+                style={{
+                  backgroundColor: "hsl(180, 52%, 96%)",
+                  color: "hsl(180, 29%, 50%)",
+                }}
+                className="px-2 py-1 mx-1 text-white font-black text-xs"
+              >
+                {job.role}
               </span>
-              <span>
-                {job.tools.map((tool) => {
-                  return <>{tool}</>;
-                })}
+              <span
+                style={{
+                  backgroundColor: "hsl(180, 52%, 96%)",
+                  color: "hsl(180, 29%, 50%)",
+                }}
+                className="px-2 py-1  mx-1 text-white font-black text-xs"
+              >
+                {job.level}
               </span>
+
+              {job.languages.map((language) => {
+                return (
+                  <span
+                    style={{
+                      backgroundColor: "hsl(180, 52%, 96%)",
+                      color: "hsl(180, 29%, 50%)",
+                    }}
+                    className="px-2 py-1  mx-1  text-white font-black text-xs"
+                  >
+                    {language}{" "}
+                  </span>
+                );
+              })}
+
+              {job.tools.map((tool) => {
+                return (
+                  <span
+                    style={{
+                      backgroundColor: "hsl(180, 52%, 96%)",
+                      color: "hsl(180, 29%, 50%)",
+                    }}
+                    className="px-2 py-1  mx-1 text-white font-black text-xs"
+                  >
+                    {tool}
+                  </span>
+                );
+              })}
             </div>
           </div>
         );
